365b99aa33f55034a896b1a8c70b6a010eda1d21
tsar-boomba <itg.2048@gmail.com>
2025-09-16 19:17:41 -0400

    Add lecture 4 demo

---
+++ demos/lecture4/index.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+
+let click = 0;
+button.addEventListener('click', (event) => {
+	const text = document.createElement('p');
+	text.innerText = 'Click ' + click;
+	container.prepend(text);
+	click++;
+});
+
+button2.addEventListener('click', async (event) => {
+	const response = await fetch('https://pokeapi.co/api/v2/pokemon/azumarill');
+	const data = await response.json();
+	const img = document.createElement('img');
+	img.src = data.sprites.front_default;
+	container.appendChild(img);
+});
+
+
